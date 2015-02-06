3cda34a18908c703200c2afe3cc147fb808848fb
Python Sandbox <PySandbox@celia-mbp.local>
2015-02-06 11:46:14 -0500

    Adding option for users to put their tokens into a config file

---
+++ notebooks/config.py
@@ -0,0 +1,6 @@
+# These are the tokens obtained from https://apps.twitter.com/
+
+CONSUMER_KEY = ""           # Consumer Key (API Key)
+CONSUMER_SECRET = ""        # Consumer Secret (API Secret)
+ACCESS_TOKEN = ""           # Access Token
+ACCESS_SECRET = ""          # Access Token Secret
